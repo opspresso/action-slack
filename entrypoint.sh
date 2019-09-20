866090f7cf0e2fa07dc6e3cc1d24a268d3194322
nalbam <me@nalbam.com>
2019-09-20 19:13:28 +0900

    lint

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,11 +19,11 @@ _slack() {
 
   URL="https://hooks.slack.com/services/${SLACK_TOKEN}"
   curl \
-      -sSL \
-      -X POST \
-      -H "Content-type: application/json" \
-      --data @"${JSON_PATH}" \
-      ${URL}
+    -sSL \
+    -X POST \
+    -H "Content-type: application/json" \
+    --data @"${JSON_PATH}" \
+    ${URL}
 }
 
 _slack
