5072843de9d0610eebf7190de05f0659386f72d4
nalbam <me@nalbam.com>
2019-09-20 17:04:04 +0900

    _slack

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,20 +2,28 @@
 
 set -e
 
-if [ -z "${SLACK_TOKEN}" ]; then
-  echo "SLACK_TOKEN is not set."
-  exit 1
-fi
+_slack_pre() {
+  if [ -z "${SLACK_TOKEN}" ]; then
+    echo "SLACK_TOKEN is not set."
+    exit 1
+  fi
 
-if [ -z "${JSON_PATH}" ] || [ ! -f "${JSON_PATH}" ]; then
-  echo "JSON_PATH is not set."
-  exit 1
-fi
+  if [ -z "${JSON_PATH}" ] || [ ! -f "${JSON_PATH}" ]; then
+    echo "JSON_PATH is not set."
+    exit 1
+  fi
+}
 
-URL="https://hooks.slack.com/services/${SLACK_TOKEN}"
-curl \
-    -sSL \
-    -X POST \
-    -H "Content-type: application/json" \
-    --data @"${JSON_PATH}" \
-    ${URL}
+_slack() {
+  _slack_pre
+
+  URL="https://hooks.slack.com/services/${SLACK_TOKEN}"
+  curl \
+      -sSL \
+      -X POST \
+      -H "Content-type: application/json" \
+      --data @"${JSON_PATH}" \
+      ${URL}
+}
+
+_slack
